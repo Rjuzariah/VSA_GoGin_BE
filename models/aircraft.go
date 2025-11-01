ad4eb29981ff51613f822b5d51a3d5994b445a75
Juzariah <riajuzariah@gmail.com>
2025-11-02 01:05:08 +0700

    update model

--- models/aircraft.go
+++ models/aircraft.go
@@ -1,11 +1,8 @@
 package models
 
-import "time"
-
 type Aircraft struct {
-	ID           uint      `json:"id" gorm:"primaryKey"`
-	AircraftType string    `json:"aircraft_type"`
-	NumRows      int       `json:"num_rows"`
-	SeatsPerRow  string    `json:"seats_per_row"` // Comma-separated seat letters, e.g. "A,C,D,F"
-	CreatedAt    time.Time `json:"created_at"`
+	ID           uint   `json:"id" gorm:"primaryKey"`
+	AircraftType string `json:"aircraft_type"`
+	NumRows      int    `json:"num_rows"`
+	SeatsPerRow  string `json:"seats_per_row"` // Comma-separated seat letters, e.g. "A,C,D,F"
 }
--- models/flight.go
+++ models/flight.go
@@ -3,10 +3,9 @@ package models
 import "time"
 
 type Flight struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`
-	FlightNumber string   `json:"flight_number"`
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	FlightNumber string    `json:"flight_number"`
 	FlightDate   time.Time `json:"flight_date"`
-	AircraftID   uint     `json:"aircraft_id"`
-	CreatedAt    time.Time `json:"created_at"`
+	AircraftID   uint      `json:"aircraft_id"`
 	Aircraft     Aircraft  `json:"aircraft" gorm:"foreignKey:AircraftID"`
 }
